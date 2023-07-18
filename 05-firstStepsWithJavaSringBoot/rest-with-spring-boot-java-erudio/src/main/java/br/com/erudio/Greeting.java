1fb7fd4c09aa4875758ab7e14041dbcdd335aced
LeandroMedeirosLima <leandromedeiros007@gmail.com>
2023-07-18 10:01:58 -0300

    First steps in Java Spring Boot

---
+++ 05-firstStepsWithJavaSringBoot/rest-with-spring-boot-java-erudio/src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,22 @@
+package br.com.erudio;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(Long id, String content) {
+        super();
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
