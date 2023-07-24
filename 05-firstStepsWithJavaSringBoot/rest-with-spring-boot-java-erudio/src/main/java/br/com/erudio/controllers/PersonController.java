9306c9d0e1072de5b63b4cb3f2aa7ce53babfcbd
LeandroMedeirosLima <leandromedeiros007@gmail.com>
2023-07-24 10:26:39 -0300

    Adição do suporte a YAML

--- 05-firstStepsWithJavaSringBoot/rest-with-spring-boot-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 05-firstStepsWithJavaSringBoot/rest-with-spring-boot-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -3,7 +3,6 @@ package br.com.erudio.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,46 +17,56 @@ import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.services.PersonService;
 
+import br.com.erudio.util.MediaType;
+
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
     @GetMapping(value = "/{id}", 
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = { MediaType.APPLICATION_JSON, 
+                         MediaType.APPLICATION_XML, 
+                         MediaType.APPLICATION_YML})
     public PersonVO findById(@PathVariable(value = "id") Long id) {
         return personService.findById(id);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(
+            produces = { MediaType.APPLICATION_JSON, 
+                         MediaType.APPLICATION_XML,
+                         MediaType.APPLICATION_YML})
     public List<PersonVO> findAll() {
         return personService.findAll();
     }
 
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, 
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(
+            produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }, 
+            consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
     public PersonVO create(@RequestBody PersonVO person) {
         return personService.create(person);
     }
-    
-    @PostMapping(value = "/v2", produces = MediaType.APPLICATION_JSON_VALUE, 
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+
+    @PostMapping(value = "/v2", 
+            produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }, 
+            consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
     public PersonVOV2 create(@RequestBody PersonVOV2 person) {
         return personService.createV2(person);
     }
-    
-    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, 
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+
+    @PutMapping(
+                produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }, 
+                consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
     public PersonVO update(@RequestBody PersonVO person) {
         return personService.update(person);
     }
-    
+
     @DeleteMapping(value = "{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         personService.delete(id);
-        
+
         return ResponseEntity.noContent().build();
     }
 }
---
+++ 05-firstStepsWithJavaSringBoot/rest-with-spring-boot-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Brasília', 'Leandro', 'Masculino', 'Lima'),
+	(2, 'Brasília', 'Rosa', 'Feminino', 'Rosa'),
+	(4, 'Vezena', 'Leonardo', 'Masculino', 'Da Vince'),
+	(6, 'Goiânia', 'Gustavo', 'Masculino', 'Lima'),
+	(7, 'Índia', 'Gandhi', 'Masculino', 'Mahatma'),
+	(8, 'Brasil', 'Joana', 'Feminino', 'Prado'),
+	(9, 'Brasil', 'Nelson', 'Masculino', 'Mandela'),
+	(10, 'Croatia', 'Nicola', 'Masculino', 'Tesla');
